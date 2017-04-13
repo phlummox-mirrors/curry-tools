eed6949a0de814bf752f8a8de47b8f0d9bc2a7c2
Michael Hanus <mh@informatik.uni-kiel.de>
2017-04-13 16:35:38 +0200

    CurryBrowser update removed

--- download_tools.sh
+++ download_tools.sh
@@ -78,47 +78,6 @@ mv Makefile.currycheck currycheck/Makefile
 echo "'currycheck' updated from package repository."
 
 ##############################################################################
-echo "Updating 'browser'..."
-mv browser/Makefile Makefile.browser  # keep old Makefile
-rm -rf browser
-cpm checkout currybrowse
-mv currybrowse browser
-cd browser
-cpm install --noexec
-rm -rf .git* package.json
-rm -rf .cpm/*_cache
-rm -rf .cpm/packages/*/.git*
-cd .cpm/packages
- CANAV=`ls -d cass-analysis-*`
- mv $CANAV cass-analysis
- CASSV=`ls -d cass-*\.*\.*`
- mv $CASSV cass
- ln -s cass-analysis $CANAV
- ln -s cass $CASSV
- PKGV=`ls -d addtypes-*`
- mv $PKGV addtypes
- ln -s addtypes $PKGV
- PKGV=`ls -d importusage-*`
- mv $PKGV importusage
- ln -s importusage $PKGV
- PKGV=`ls -d showflatcurry-*`
- mv $PKGV showflatcurry
- ln -s showflatcurry $PKGV
-cd ../..
-# Generate package configuration file:
-CBCONFIG=src/BrowsePackageConfig.curry
-echo "module BrowsePackageConfig where"  > $CBCONFIG
-echo "import Distribution(installDir)"  >> $CBCONFIG
-echo "import FilePath(combine)"         >> $CBCONFIG
-echo "packageVersion :: String"         >> $CBCONFIG
-echo "packageVersion = \"0.5.0\""       >> $CBCONFIG
-echo "packagePath :: String"            >> $CBCONFIG
-echo "packagePath = combine installDir (combine \"currytools\" \"browser\")" >> $CBCONFIG
-cd ..
-mv Makefile.browser browser/Makefile
-echo "'browser' updated from package repository."
-
-##############################################################################
 echo "Updating 'optimize'..."
 mv optimize/Makefile Makefile.optimize  # keep old Makefile
 rm -rf optimize
